9cb926df7b50b6a8680b8067758ed414c91496f4
mylokaku-star <email@gmail.com>
2026-06-28 21:36:51 +0700

    promo even

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import { Routes, Route, Navigate, useLocation } from 'react-router-dom'
 import { useEffect } from 'react'
 import { Toaster } from 'sonner'
+import ProtectedRoute from './components/ProtectedRoute'
+import RefreshButton from './components/RefreshButton'
 import LandingPage from './pages/LandingPage'
 import LoginPage from './pages/LoginPage'
 import LupaPasswordPage from './pages/LupaPasswordPage'
@@ -19,6 +21,7 @@ import AdminPage from './pages/AdminPage'
 import PusatBantuan from './pages/PusatBantuan'
 import VerifikasiWA from './pages/VerifikasiWA'
 import BuatPromoPage from './pages/BuatPromoPage'
+import DetailPromoPage from './pages/DetailPromoPage'
 import KeranjangPage from './pages/KeranjangPage'
 
 function ScrollToTop() {
@@ -32,33 +35,39 @@ function App() {
     <>
       <Toaster position="top-center" richColors />
       <ScrollToTop />
+      <RefreshButton />
       <Routes>
+        {/* Bebas diakses */}
         <Route path="/" element={<LandingPage />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/lupa-password" element={<LupaPasswordPage />} />
-        <Route path="/register" element={<Navigate to="/login" replace />} />
+        <Route path="/register" element={<Navigate to="/login?tab=daftar" replace />} />
         <Route path="/cari" element={<CariTokoPage />} />
         <Route path="/peta" element={<PetaPage />} />
         <Route path="/toko/:id" element={<DetailTokoPage />} />
+        <Route path="/promo/:id" element={<DetailPromoPage />} />
         <Route path="/verifikasi-wa" element={<VerifikasiWA />} />
-        <Route path="/dashboard" element={<DashboardPage />} />
-        <Route path="/buat-toko" element={<BuatTokoPage />} />
-        
-        {/* Rute Dinamis */}
-        <Route path="/buat-promo/:id" element={<BuatPromoPage />} />
-        <Route path="/edit-toko/:id" element={<EditTokoPage />} />
-        <Route path="/kelola-produk/:id" element={<ProdukPage />} />
-        
-        {/* Menggunakan ChatPage untuk list chat penjual */}
-        <Route path="/chat-list-penjual/:id" element={<ChatPage />} />
-        
-        <Route path="/tambah-produk" element={<TambahProdukPage />} />
-        <Route path="/profil" element={<ProfilPage />} />
-        <Route path="/chat/:tokoId" element={<ChatPage />} />
-        <Route path="/verifikasi" element={<VerifikasiPage />} />
-        <Route path="/admin" element={<AdminPage />} />
-        <Route path="/pusat-bantuan" element={<PusatBantuan />} />
-        <Route path="/keranjang" element={<KeranjangPage />} />
+        <Route path="/preloved" element={<Navigate to="/cari?kategori=preloved" replace />} />
+
+        {/* Redirect route lama ke route baru yang konsisten */}
+        <Route path="/edit-toko/:id" element={<Navigate to="/edit-toko" replace />} />
+        <Route path="/kelola-produk/:id" element={<Navigate to="/produk" replace />} />
+        <Route path="/buat-promo/:id" element={<Navigate to="/buat-promo" replace />} />
+        <Route path="/chat-list-penjual/:id" element={<Navigate to={'/chat/:id'.replace(':id', '')} replace />} />
+
+        {/* Perlu login */}
+        <Route path="/dashboard" element={<ProtectedRoute><DashboardPage /></ProtectedRoute>} />
+        <Route path="/buat-toko" element={<ProtectedRoute><BuatTokoPage /></ProtectedRoute>} />
+        <Route path="/buat-promo" element={<ProtectedRoute><BuatPromoPage /></ProtectedRoute>} />
+        <Route path="/tambah-produk" element={<ProtectedRoute><TambahProdukPage /></ProtectedRoute>} />
+        <Route path="/edit-toko" element={<ProtectedRoute><EditTokoPage /></ProtectedRoute>} />
+        <Route path="/produk" element={<ProtectedRoute><ProdukPage /></ProtectedRoute>} />
+        <Route path="/profil" element={<ProtectedRoute><ProfilPage /></ProtectedRoute>} />
+        <Route path="/chat/:tokoId" element={<ProtectedRoute><ChatPage /></ProtectedRoute>} />
+        <Route path="/verifikasi" element={<ProtectedRoute><VerifikasiPage /></ProtectedRoute>} />
+        <Route path="/admin" element={<ProtectedRoute><AdminPage /></ProtectedRoute>} />
+        <Route path="/pusat-bantuan" element={<ProtectedRoute><PusatBantuan /></ProtectedRoute>} />
+        <Route path="/keranjang" element={<ProtectedRoute><KeranjangPage /></ProtectedRoute>} />
       </Routes>
     </>
   )
